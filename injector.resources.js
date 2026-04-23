6a4e008887c8bec23892d75d6130294e44fe036c
Afton Martin <aftonam@gmail.com>
2026-04-22 23:12:58 -0500

    Added scrollable and bogus data

--- injector.resources.js
+++ injector.resources.js
@@ -6,5 +6,82 @@ window.INJECTOR_RESOURCES = [
   {
     label: "Insert Goodbye World",
     text: "Goodbye World"
+  },
+  {
+    label: "Insert Alpha",
+    text: "Alpha"
+  },
+  {
+    label: "Insert Bravo",
+    text: "Bravo"
+  },
+  {
+    label: "Insert Charlie",
+    text: "Charlie"
+  },
+  {
+    label: "Insert Delta",
+    text: "Delta"
+  },
+  {
+    label: "Insert Echo",
+    text: "Echo"
+  },
+  {
+    label: "Insert Foxtrot",
+    text: "Foxtrot"
+  },
+  {
+    label: "Insert Golf",
+    text: "Golf"
+  },
+  {
+    label: "Insert Hotel",
+    text: "Hotel"
+  },
+  {
+    label: "Insert India",
+    text: "India"
+  },
+  {
+    label: "Insert Juliet",
+    text: "Juliet"
+  },
+  {
+    label: "Insert Kilo",
+    text: "Kilo"
+  },
+  {
+    label: "Insert Lima",
+    text: "Lima"
+  },
+  {
+    label: "Insert Mike",
+    text: "Mike"
+  },
+  {
+    label: "Insert November",
+    text: "November"
+  },
+  {
+    label: "Insert Oscar",
+    text: "Oscar"
+  },
+  {
+    label: "Insert Papa",
+    text: "Papa"
+  },
+  {
+    label: "Insert Quebec",
+    text: "Quebec"
+  },
+  {
+    label: "Insert Romeo",
+    text: "Romeo"
+  },
+  {
+    label: "Insert Sierra",
+    text: "Sierra"
   }
+  
 ];
