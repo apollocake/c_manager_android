ebc60ad818db8c292cb1749aaace01044052c2c7
Afton Martin <aftonam@gmail.com>
2026-04-23 00:31:54 -0500

    removed unused example file

--- borderify.js
+++
@@ -1,8 +0,0 @@
-console.log("borderify running on", location.href);
-debugger;
-
-const BORDER_COLOR = "#ff9500";
-const BORDER_WIDTH = "4px";
-
-document.documentElement.style.boxSizing = "border-box";
-document.documentElement.style.border = `${BORDER_WIDTH} solid ${BORDER_COLOR}`;
